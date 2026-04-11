010f0b14878fdd8a42e3b1e3d18df10e8274835d
Gopilal Anjaniya <gopilalanjaniya@gmail.com>
2026-04-11 12:52:44 +0530

    some change files

--- frontend/src/hooks/useGetRealTimeMessage.jsx
+++ frontend/src/hooks/useGetRealTimeMessage.jsx
@@ -1,16 +1,21 @@
 import { useEffect } from "react";
-import {useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { setMessages } from "../redux/messageSlice";
 
 const useGetRealTimeMessage = () => {
-    const {socket} = useSelector(store=>store.socket);
-    const {messages} = useSelector(store=>store.message);
-    const dispatch = useDispatch();
-    useEffect(() => {
-      socket?.on("newMessage", (newMessage) => {
-        dispatch(setMessages([...messages, newMessage]));
-      });
-      return () => socket?.off("newMessage");
-    }, [setMessages, messages, dispatch, socket]);
+  const { socket } = useSelector((store) => store.socket);
+  const { messages } = useSelector((store) => store.message);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on("newMessage", (newMessage) => {
+      dispatch(setMessages([...messages, newMessage]));
+    });
+
+    return () => socket.off("newMessage");
+  }, [messages, dispatch, socket]);  
 };
+
 export default useGetRealTimeMessage;
